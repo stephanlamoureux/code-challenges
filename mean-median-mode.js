f789b61affaceb98d4b2e73ab5ef1e5beedcf806
Stephan Lamoureux <sa.lamoureux@gmail.com>
2022-08-08 18:42:58 -0400

    Create mean-median-mode.js

---
+++ mean-median-mode.js
@@ -0,0 +1,54 @@
+// Median
+function median(arr) {
+	arr.sort((a, b) => a - b)
+	let median
+	// odd number in array so there's only 1 middle number
+	if (arr.length % 2 !== 0) {
+		median = arr[Math.floor(arr.length / 2)]
+	} else {
+		// even number in array, has 2 middle numbers
+		let middle1 = arr[arr.length / 2]
+		let middle2 = arr[arr.length / 2 - 1]
+		median = (middle1 + middle2) / 2
+	}
+	return median
+}
+
+// Mean
+function mean(arr) {
+	let sum = 0
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i]
+	}
+	return sum / arr.length
+}
+
+// Mode
+function mode(arr) {
+	const modeObj = {}
+	let max = 0,
+		count = 0
+
+	// Iterate through the array, adding each number to the JavaScript Object if it is not already there
+	for (let i = 0; i < arr.length; i++) {
+		const item = arr[i]
+		// If the number has already been added, increment its key count
+		if (modeObj[item]) {
+			modeObj[item]++
+		} else {
+			modeObj[item] = 1
+		}
+		// Check if the highest stored mode (count) is smaller than the processed number
+		if (count < modeObj[item]) {
+			// If smaller, then the count variable is changed to whatever the key for the current element is and the max value is changed to the current element. If greater, then do nothing.
+			max = item
+			count = modeObj[item]
+		}
+	}
+  // After the iteration is finished, max stores a number and count - the frequency, so we return max
+	return max
+}
+
+console.log(mean([1, 2, 3, 4]))
+console.log(median([1, 2, 3, 3, 4]))
+console.log(mode([12, 1, 3, 3, 4, 6, 7, 7, 7]))
