e6fdf99f9de033b2b9279b658becafb253dd1732
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-04-23 12:35:58 -0400

    modified:   multiplicative_persistence.py

--- multiplicative_persistence.py
+++ multiplicative_persistence.py
@@ -1,4 +1,6 @@
-# Have the function MathChallenge(num) take the num parameter being passed which will always be a positive integer and return its multiplicative persistence which is the number of times you must multiply the digits in num until you reach a single digit. For example: if num is 39 then your program should return 3 because 3 * 9 = 27 then 2 * 7 = 14 and finally 1 * 4 = 4 and you stop at 4.
+# Have the function MathChallenge(num) take the num parameter being passed which will always be a positive integer and return its multiplicative persistence which is the number of times you must multiply the digits in num until you reach a single digit.
+
+# For example: if num is 39 then your program should return 3 because 3 * 9 = 27 then 2 * 7 = 14 and finally 1 * 4 = 4 and you stop at 4.
 
 from functools import reduce
 
